d73b0caf95cba8e2d928ec9872f9ad8d08f24d46
Marcin <marcinmoskala@gmail.com>
2017-11-22 18:11:55 +0100

    Add test "When onCreate, loads and displays list of news"

--- android/app/src/test/java/com/marcinmoskala/kotlinacademy/NewsPresenterUnitTest.kt
+++ android/app/src/test/java/com/marcinmoskala/kotlinacademy/NewsPresenterUnitTest.kt
@@ -26,7 +26,7 @@ class NewsPresenterUnitTest {
     }
 
     @Test
-    fun `Presenter is using PeriodicCaller to refresh list since onCreate`() {
+    fun `Is using PeriodicCaller to refresh list since onCreate`() {
         val view = NewsView()
         var periodicCallerStarts: List<Long> = listOf()
         overridePeriodicCaller { timeMillis, callback ->
@@ -41,33 +41,49 @@ class NewsPresenterUnitTest {
         assertEquals(NewsPresenter.AUTO_REFRESH_TIME_MS, periodicCallerStarts[0])
     }
 
-    fun overrideNewsRepository(getNewsData: () -> NewsData) {
+    @Test
+    fun `When onCreate, loads and displays list of news`() {
+        val view = NewsView()
+        val newsList = listOf(FAKE_NEWS)
+        overrideNewsRepository { NewsData(newsList) }
+        val presenter = NewsPresenter(view)
+        // When
+        presenter.onCreate()
+        // Then
+        assertEquals(newsList, view.newsList)
+        assertEquals(0, view.displayedErrors.size)
+    }
+
+    private fun overrideNewsRepository(getNewsData: () -> NewsData) {
         NewsRepository.override = object : NewsRepository {
             suspend override fun getNewsData(): NewsData = getNewsData()
         }
     }
 
-    fun overridePeriodicCaller(start: (timeMillis: Long, callback: () -> Unit) -> Cancellable) {
+    private fun overridePeriodicCaller(start: (timeMillis: Long, callback: () -> Unit) -> Cancellable) {
         PeriodicCaller.override = object : PeriodicCaller {
             override fun start(timeMillis: Long, callback: () -> Unit) = start(timeMillis, callback)
         }
     }
 
-    fun NewsView(
-            onShowList: (news: List<News>) -> Unit = {},
-            onShowError: (Throwable) -> Unit = {}
-    ) = object : NewsView {
+    private fun NewsView() = object : NewsView {
         override var loading: Boolean = false
         override var swipeRefresh: Boolean = false
+        var newsList: List<News>? = null
+        var displayedErrors: List<Throwable> = emptyList()
 
         override fun showList(news: List<News>) {
-            onShowList(news)
+            newsList = news
         }
 
         override fun showError(throwable: Throwable) {
-            onShowError(throwable)
+            displayedErrors += throwable
         }
     }
 
-    fun Cancellable() = object : Cancellable {}
+    private fun Cancellable() = object : Cancellable {}
+
+    companion object {
+        val FAKE_NEWS = News(1, "Some title", "Description", "Image url", "Url")
+    }
 }
