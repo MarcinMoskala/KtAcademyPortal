31c56f4efadbd1287ef73420a2f0fb3b5dedac0d
Marcin Moskala <marcinmoskala@gmail.com>
2018-01-25 06:18:03 +0100

    Clean-up http methods

--- common-client-js/src/main/kotlin/org/kotlinacademy/ajaxUtil.kt
+++ common-client-js/src/main/kotlin/org/kotlinacademy/ajaxUtil.kt
@@ -3,35 +3,25 @@ package org.kotlinacademy
 import org.w3c.xhr.XMLHttpRequest
 import kotlin.coroutines.experimental.suspendCoroutine
 
-suspend fun httpGet(url: String): String = suspendCoroutine { c ->
-    val xhr = XMLHttpRequest()
-    xhr.onreadystatechange = {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            if (xhr.status / 100 == 2) {
-                c.resume(xhr.response as String)
-            } else {
-                c.resumeWithException(Throwable("HTTP error: ${xhr.status}"))
-            }
-        }
-        null
-    }
-    xhr.open("GET", url)
-    xhr.send()
-}
+suspend fun httpGet(url: String): String = http("GET", url)
 
-suspend fun httpPost(body: String, url: String): String = suspendCoroutine { c ->
+suspend fun httpPost(body: String, url: String): String = http("POST", url, body, "Content-Type" to "application/json")
+
+private suspend inline fun <reified T : Any> http(method: String, url: String, body: String? = null, vararg headers: Pair<String, String>): T = suspendCoroutine { c ->
     val xhr = XMLHttpRequest()
     xhr.onreadystatechange = {
         if (xhr.readyState == XMLHttpRequest.DONE) {
             if (xhr.status / 100 == 2) {
-                c.resume(xhr.response as String)
+                c.resume(xhr.response as T)
             } else {
                 c.resumeWithException(Throwable("HTTP error: ${xhr.status}"))
             }
         }
         null
     }
-    xhr.open("POST", url)
-    xhr.setRequestHeader("Content-Type", "application/json")
-    xhr.send(body)
+    xhr.open(method, url)
+    for ((key, value) in headers)
+        xhr.setRequestHeader(key, value)
+
+    if (body == null) xhr.send() else xhr.send(body)
 }
