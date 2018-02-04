dae01c6a0cee76f604d86f93a0364beb6295beb5
Marcin Moskala <marcinmoskala@gmail.com>
2018-02-04 17:17:50 +0100

    Remove serializer workaround

--- common-client-js/src/main/kotlin/org/kotlinacademy/respositories/NewsRepositoryImpl.kt
+++ common-client-js/src/main/kotlin/org/kotlinacademy/respositories/NewsRepositoryImpl.kt
@@ -1,33 +1,14 @@
 package org.kotlinacademy.respositories
 
 import org.kotlinacademy.Endpoints
-import org.kotlinacademy.data.News
 import org.kotlinacademy.data.NewsData
 import org.kotlinacademy.httpGet
 import org.kotlinacademy.json
-import org.kotlinacademy.parseDate
-
-external val JSON: dynamic
 
 class NewsRepositoryImpl : NewsRepository {
     override suspend fun getNewsData(): NewsData {
         val str = httpGet(Endpoints.news)
-        // Error
-        // val parsed = json.parse<NewsData>(str)
-        // Workaround
-        val parsed = JSON.parse(str)
-        val news = parsed.news
-        var newsList = listOf<News>()
-        for (n in news) {
-            newsList += News(
-                    id = n.id,
-                    title = n.title.toString(),
-                    subtitle = n.subtitle.toString(),
-                    imageUrl = n.imageUrl.toString(),
-                    url = n.url?.toString(),
-                    occurrence = n.occurrence.toString().parseDate()
-            )
-        }
-        return NewsData(newsList)
+        val parsed = json.parse<NewsData>(str)
+        return NewsData(parsed.news)
     }
 }
