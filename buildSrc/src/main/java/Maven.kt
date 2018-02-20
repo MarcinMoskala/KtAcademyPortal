b73351cd67aa31de3f5c6ae86329c2c354a50ab9
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2018-02-20 17:40:26 +0100

    Use kotlin+buildSrc to manage dependencies
    more kotlin, better IDE integration

---
+++ buildSrc/src/main/java/Maven.kt
@@ -0,0 +1,16 @@
+object Maven {
+
+  const val KotlinWrappers = "http://dl.bintray.com/hypnosphi/kotlin-wrappers"
+  const val KotlinDev = "http://dl.bintray.com/kotlin/kotlin-dev"
+  const val KotlinEap = "http://dl.bintray.com/kotlin/kotlin-eap"
+  const val KotlinEap12 = "http://dl.bintray.com/kotlin/kotlin-eap-1.2"
+  const val Kotlinx =  "https://kotlin.bintray.com/kotlinx"
+  const val Ktor = "https://dl.bintray.com/kotlin/ktor"
+  const val Squash = "https://dl.bintray.com/kotlin/squash"
+  const val Sargunster = "https://dl.bintray.com/sargunster/maven"
+  const val Jitpack = "https://jitpack.io"
+  const val Google = "https://maven.google.com"
+  const val SonatypeSnapshots = "https://oss.sonatype.org/content/repositories/snapshots"
+  const val GradlePlugins = "https://plugins.gradle.org/m2/"
+
+}
