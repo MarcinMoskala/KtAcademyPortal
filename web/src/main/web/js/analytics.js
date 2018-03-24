ba6a85a2ee91ec2bf43a7c32a0015ec3a00ad4ef
Marcin Moskala <marcinmoskala@gmail.com>
2018-03-24 09:48:08 +0200

    Web: Add Google Analytics

---
+++ web/src/main/web/js/analytics.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-92159206-12"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-92159206-12');
+</script>
