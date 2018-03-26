7feaa7ddf87df141ced2422ad3c302c2cd535ae5
Marcin Moskala <marcinmoskala@gmail.com>
2018-03-26 21:40:47 +0200

    Web: Fix analytics

--- web/src/main/web/js/analytics.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-92159206-12"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-92159206-12');
-</script>
