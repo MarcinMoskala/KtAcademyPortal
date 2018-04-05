a76cfd760d1e61815338e6de353dbb11c9fc0db0
Marcin Moskala <marcinmoskala@gmail.com>
2018-04-05 17:20:13 +0200

    Common JS: Repacking in Serialization lib is not necessary anymore

--- common-client-js/src/main/kotlin/org/kotlinacademy/respositories/ManagerRepositoryImpl.kt
+++ common-client-js/src/main/kotlin/org/kotlinacademy/respositories/ManagerRepositoryImpl.kt
@@ -6,7 +6,7 @@ import org.kotlinacademy.Endpoints.news
 import org.kotlinacademy.Endpoints.propositions
 import org.kotlinacademy.Endpoints.puzzler
 import org.kotlinacademy.Endpoints.reject
-import org.kotlinacademy.data.*
+import org.kotlinacademy.data.NewsData
 import org.kotlinacademy.httpGet
 import org.kotlinacademy.httpPost
 import org.kotlinacademy.json
@@ -29,13 +29,6 @@ class ManagerRepositoryImpl : ManagerRepository {
         httpPost("$puzzler/$id/$reject?Secret-hash=$secret")
     }
 
-    override suspend fun getPropositions(secret: String): NewsData {
-        val str = httpGet("$news/$propositions?Secret-hash=$secret")
-        val parsed = json.parse<NewsData>(str)
-        val articles = parsed.articles.map { Article(it.id, ArticleData(it.data.title, it.data.subtitle, it.data.imageUrl, it.data.url, it.data.occurrence), it.dateTime) }
-        val infos = parsed.infos.map { Info(it.id, InfoData(it.data.title, it.data.imageUrl, it.data.description, it.data.sources, it.data.url, it.data.author, it.data.authorUrl), it.dateTime, it.accepted) }
-        val puzzlers = parsed.puzzlers.map { Puzzler(it.id, PuzzlerData(it.data.title, it.data.level, it.data.question, it.data.answers, it.data.correctAnswer, it.data.explanation, it.data.author, it.data.authorUrl), it.dateTime, it.accepted) }
-        val newsData = NewsData(articles, infos, puzzlers)
-        return newsData
-    }
+    override suspend fun getPropositions(secret: String): NewsData =
+            json.parse(httpGet("$news/$propositions?Secret-hash=$secret"))
 }
--- common-client-jvm/src/main/kotlin/org/kotlinacademy/respositories/PuzzlerRepositoryImpl.kt
+++ common-client-jvm/src/main/kotlin/org/kotlinacademy/respositories/PuzzlerRepositoryImpl.kt
@@ -12,7 +12,6 @@ import retrofit2.http.POST
 import ru.gildor.coroutines.retrofit.await
 
 class PuzzlerRepositoryImpl : PuzzlerRepository {
-
     private val api = retrofit.create(Api::class.java)!!
 
     override suspend fun propose(puzzler: PuzzlerData) {
@@ -21,6 +20,10 @@ class PuzzlerRepositoryImpl : PuzzlerRepository {
         } catch (t: HttpException) {
             throw HttpError(t.code(), t.message())
         }
+    }
+
+    override suspend fun update(puzzler: Puzzler, secret: String) {
+        TODO("not implemented") // Implement when it will be used
     }
 
     interface Api {
