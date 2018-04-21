1f639fcbc59ea5ed1dcbace45d00a38573cb6e83
Marcin Moskala <marcinmoskala@gmail.com>
2018-04-21 14:15:55 +0200

    Android Wear: Fix version format

--- common/src/main/kotlin/org/kotlinacademy/Headers.kt
+++ common/src/main/kotlin/org/kotlinacademy/Headers.kt
@@ -2,5 +2,5 @@ package org.kotlinacademy
 
 object Headers {
     val androidMobileMinVersion = "android-mobile-min-version"
-    val androidWearMinVersion = "android-mobile-min-version"
+    val androidWearMinVersion = "android-wear-min-version"
 }
