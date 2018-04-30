12988ffae4c27df9683b1ddee72f65881226cdf1
Marcin Moskala <marcinmoskala@gmail.com>
2018-04-30 17:14:42 +0200

    Web: Introduce "Embedding Kotlin Playground" as a Puzzler code

--- web/src/main/kotlin/org/kotlinacademy/common/CodeFormatting.kt
+++ web/src/main/kotlin/org/kotlinacademy/common/CodeFormatting.kt
@@ -3,10 +3,6 @@ package org.kotlinacademy.common
 import org.w3c.dom.asList
 import kotlin.browser.document
 
-private external val hljs: dynamic
-
 fun applyCodeHighlighting() {
-    document.getElementsByClassName("kotlin").asList().forEach {
-        hljs.highlightBlock(it)
-    }
+    js("KotlinPlayground('.kotlin');")
 }
