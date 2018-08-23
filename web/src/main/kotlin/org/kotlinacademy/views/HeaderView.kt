2bda26b331f20f72915382c7c33745fbda0f3fb0
Marcin Moskala <marcinmoskala@gmail.com>
2018-08-23 18:12:29 +0100

    Add navigation

--- web/src/main/kotlin/org/kotlinacademy/views/HeaderView.kt
+++ web/src/main/kotlin/org/kotlinacademy/views/HeaderView.kt
@@ -1,20 +1,58 @@
 package org.kotlinacademy.views
 
+import kotlinx.html.id
+import kotlinx.html.js.onClickFunction
+import kotlinx.html.onClick
+import org.w3c.dom.events.Event
 import react.RBuilder
 import react.ReactElement
-import react.dom.div
-import react.dom.h1
-import react.dom.h2
+import react.dom.*
+import kotlin.browser.document
 
 fun RBuilder.headerView(): ReactElement? = div(classes = "header") {
-    div(classes = "header-content") {
-        div(classes = "list-center") {
-            h1(classes = "header-title default-font") {
-                +"Kot. Academy"
+
+    div(classes = "navigation-bar") {
+        div(classes = "logo-container") {
+            a(target = "_top", href = "", classes = "pointer logo-img") {
+                img(src = "img/logo_full.png", alt = "Kot. Academy Logo") {
+                    attrs { width = "122,8"; height = "70,6" }
+                }
             }
-            h2(classes = "header-subtitle default-font") {
-                +"With mission to simplify Kotlin learning"
+        }
+        nav(classes = "bookmarks") {
+            attrs { id = "bookmarks" }
+            ul {
+                menuItem("Home", "http://kot.academy", first = true)
+                menuItem("Portal", "http://portal.kot.academy")
+                menuItem("Blog", "http://blog.kotlin-academy.com")
+                menuItem("Contact", "http://kot.academy/#contact")
+                li(classes = "inline") {
+                    a(href = "javascript:void(0);", classes = "menu-icon") {
+                        attrs { onClickFunction = ::onHamburgerClicked }
+                        i(classes = "fas fa-bars") { }
+                    }
+                }
             }
         }
     }
+
+    div(classes = "banner") {
+        div(classes = "wow fadeInDown") {
+            h1 {
+                +"KOT. ACADEMY"
+            }
+            h3 {
+                +"We simplify KOTLIN learning"
+            }
+        }
+    }
+}
+
+private fun RBuilder.menuItem(text: String, href: String, first: Boolean = false): ReactElement? = li(classes = "inline") {
+    a(href = href, classes = "nav-link pointer" + if (first) " first-bookmark" else "") { +text }
+}
+
+fun onHamburgerClicked(e: Event) {
+    val bookmarksView = document.getElementById("bookmarks")
+    bookmarksView?.className = if (bookmarksView?.className == "bookmarks") "bookmarks responsive" else "bookmarks"
 }
